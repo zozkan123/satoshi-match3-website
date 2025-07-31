d82979dedc9286a61a4df3fcbf5d2ab50d4cb6e5
zozkan123 <159782197+zozkan123@users.noreply.github.com>
2025-08-01 01:35:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: ['satoshi-match3-website.onrender.com'],
   },
 })
